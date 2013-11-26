e64b16b84d5fb04b51bcd86c9543ec5b7d5b56e1
Alexandr Evseev <evseev@aboutecho.com>
2013-11-26 14:44:02 +0400

    Stream&Submit controls added

--- app.js
+++ app.js
@@ -5,8 +5,51 @@ var $ = jQuery;
 
 if (Echo.App.isDefined("Echo.Apps.Conversations")) return;
 
-var conversation = Echo.App.manifest("Echo.Apps.Conversations");
+var conversations = Echo.App.manifest("Echo.Apps.Conversations");
 
-Echo.App.create(conversation);
+conversations.config = {
+	"targetURL": ""
+};
+
+conversations.dependencies = [{
+	"url": "{config:cdnBaseURL.sdk}/streamserver.pack.js",
+	"loaded": function() {
+		return Echo.App.isDefined("Echo.StreamServer.Controls.Submit")
+			&& Echo.Control.isDefined("Echo.StreamServer.Controls.Stream");
+	}
+}];
+
+conversations.templates.main =
+	'<div class="{class:container}">' +
+		'<div class="{class:submit}"></div>' +
+		'<div class="{class:stream}"></div>' +
+	'</div>';
+
+conversations.renderers.submit = function(element) {
+	this.initComponent({
+		"id": "stream",
+		"component": "Echo.StreamServer.Controls.Submit",
+		"config": {
+			"target": element
+		}
+	});
+};
+
+conversations.renderers.stream = function(element) {
+	this.initComponent({
+		"id": "Stream",
+		"component": "Echo.StreamServer.Controls.Stream",
+		"config": {
+			"target": element,
+			"query": this._buildSearchQuery()
+		}
+	});
+};
+
+conversations.methods._buildSearchQuery = function() {
+	return "childrenof:" + this.config.get("targetURL") + " type:comment state:Approved,Untouched";
+};
+
+Echo.App.create(conversations);
 
 })(Echo.jQuery);
