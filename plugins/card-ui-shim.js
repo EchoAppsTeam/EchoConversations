977cf0260cd4167990208771d7277f5ff8f2d1d1
Alexandr Evseev <evseev@aboutecho.com>
2013-12-03 18:38:56 +0400

    Minor fix

--- plugins/card-ui-shim.js
+++ plugins/card-ui-shim.js
@@ -111,7 +111,7 @@ plugin.css =
 	'.{plugin.class} .{class:body} { padding-top: 0px; margin-bottom: 8px; }' +
 	'.{plugin.class} .{class:body} .{class:text} { color: #262626; font-size: 13px; line-height: 17px; font-family: Georgia, "Times New Roman", Times, serif; }' +
 	'.{plugin.class} .{class:authorName} { color: #595959; font-weight: normal; font-size: 14px; line-height: 16px; }' +
-	'.{plugin.class} .{class:content} .{class:container-child-thread} { padding: 0px 16px 0px 16px; margin-top: 20px; }' +
+	'.{plugin.class} .{class:content} .{class:container-child-thread} { padding: 0px 0px 0px 16px; margin-top: 20px; }' +
 	'.{plugin.class} .{class:children} .{class:avatar-wrapper} { margin-top: 5px; }' +
 	'.{plugin.class} .{class:children} .{class:frame} { margin-left: 5px; }' +
 	'.{plugin.class} .{class:children} .{class:data} { margin-top: 2px; padding-top: 0px; }' +
