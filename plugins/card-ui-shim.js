4b98987d7e118222a4a74c7dbbc77ef946735884
Dmitry Bragin <dbragin@aboutecho.com>
2013-12-12 15:57:18 +0400

    Some CSS fixes for item footer & children container

--- plugins/card-ui-shim.js
+++ plugins/card-ui-shim.js
@@ -309,12 +309,12 @@ plugin.css =
 	'.{plugin.class} .{class:body} { padding-top: 0px; margin-bottom: 8px; }' +
 	'.{plugin.class} .{class:body} .{class:text} { color: #42474A; font-size: 15px; line-height: 21px; }' +
 	'.{plugin.class} .{class:authorName} { color: #595959; font-weight: normal; font-size: 14px; line-height: 16px; }' +
-	'.{plugin.class} .{class:content} .{class:container-child-thread} { padding: 0px 0px 0px 8px; padding-top: 20px; }' +
 	'.{plugin.class} .{class:children} .{class:avatar-wrapper} { margin-top: 5px; }' +
 	'.{plugin.class} .{class:children} .{class:frame} { margin-left: 5px; }' +
 	'.{plugin.class} .{class:children} .{class:data} { margin-top: 2px; padding-top: 0px; }' +
 	'.{plugin.class} .{class:children} .{plugin.class:wrapper} { padding-top: 0px; background: none; border: none; }' +
-	'.{plugin.class} .{class:container-child} { padding: 0px 0px 0px 16px; padding-top: 20px; }' +
+	'.{plugin.class} .{class:container-child} { padding: 20px 0px 0px 16px; margin: 0px 15px 2px 0px; }' +
+	'.{plugin.class} .{class:content} .{class:container-child-thread} { padding: 20px 0px 0px 8px; margin: 0px 15px 2px 0px; }' +
 
 	'.{plugin.class} .{class:button} { margin-right: 10px; }' +
 	'.{plugin.class} .{class:button-delim} { display: none; }' +
@@ -334,7 +334,7 @@ plugin.css =
 	'.{plugin.class} .{class:expandChildren}:hover .{plugin.class:chevron} { opacity: 0.8; }' +
 	'.{plugin.class} .{class:expandChildren} .echo-message-icon { padding-left: 0px; background: none; }' +
 	'.{plugin.class} .{class:expandChildren} .{class:message-loading} { background: none; }' +
-	'.{plugin.class} .{class:depth-0} .{class:footer} { padding-top: 8px; height: 30px; }' +
+	'.{plugin.class} .{class:depth-0} .{class:footer} { padding: 8px 0px 10px; }' +
 	'.{plugin.class} .{class:depth-0} .{class:body} { padding-top: 0px; }' +
 	'.{plugin.class} .{class:depth-0} .{class:avatar} { height: 36px; width: 36px; }' +
 	'.{plugin.class} .{class:depth-0} .{class:avatar} img { height: 36px; width: 36px; border-radius: 50%;}' +
