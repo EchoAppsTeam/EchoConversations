12a2aabd9399674fcfc0036987b2ee95657cfc60
Alexandr Evseev <evseev@aboutecho.com>
2014-01-24 15:24:24 +0400

    comment fixed

--- plugins/card-ui-url-resolver.js
+++ plugins/card-ui-url-resolver.js
@@ -358,7 +358,7 @@ submitPlugin.methods._getMediaAttachments = function() {
 
 submitPlugin.methods.getURLs = function(text) {
 	var reURL = /(https?:\/\/)\S+\.\S{2,}|(https?:\/\/)?\S+\.\w{2,}\/\S*/;
-	var reSearch = new RegExp(reURL.source, "g"); // add global identificator
+	var reSearch = new RegExp(reURL.source, "g"); // add global modificator
 	return $.map(text.match(reSearch) || [], function(url) {
 		var matches = url.match(reURL);
 		return (!matches[1] && !matches[2])
