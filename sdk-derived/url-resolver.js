1d27701c090e54aad9c9fb2d6abd41f86cc972e1
Valeriy Shkiryak <lizard@aboutecho.com>
2014-04-04 15:18:22 +0400

    Method to extract URLs from a string

--- sdk-derived/url-resolver.js
+++ sdk-derived/url-resolver.js
@@ -4,7 +4,7 @@
 var cache = {};
 
 Echo.URLResolver = function(config) {
-	this.config = new Echo.Configuration(config);
+	this.config = new Echo.Configuration(config || {});
 };
 
 Echo.URLResolver.prototype.resolve = function(urls, callback) {
@@ -21,10 +21,13 @@ Echo.URLResolver.prototype.resolve = function(urls, callback) {
 };
 
 // at least "http://a.bc" or "a.bc/" but more likely "http://domain.com/some/path"
-var reURL = /^(https?:\/\/)\S+\.\S{2,}$|^(https?:\/\/)?\S+\.\w{2,}\/\S*$/;
+var mask1 = "(https?://)\\S+\\.\\S{2,}";
+var mask2 = "(https?://)?\\S+\\.\\w{2,}/\\S*";
+var reValidURL = new RegExp("^" + mask1 + "$|^" + mask2 + "$");
+var reURLSearch = new RegExp(mask1 + "(?=\\s|$)|(?:^|\\s)" + mask2, "g");
 
 Echo.URLResolver.prototype.normalizeURL = function(url) {
-	var matches = url.match(reURL);
+	var matches = url.match(reValidURL);
 	if (!matches) return "";
 	if (!matches[1] && !matches[2]) {
 		url = "http://" + url;
@@ -32,6 +35,12 @@ Echo.URLResolver.prototype.normalizeURL = function(url) {
 	return url;
 };
 
+Echo.URLResolver.prototype.extractURLs = function(text) {
+	var self = this;
+	return $.map(text.match(reURLSearch) || [], function(url) {
+		return self.normalizeURL($.trim(url));
+	});
+};
 
 Echo.URLResolver.prototype._resolve = function(urls, callback) {
 	var self = this, unresolvedURLs = [];
@@ -65,7 +74,7 @@ Echo.URLResolver.prototype._resolve = function(urls, callback) {
 		if (data.type === "error") return;
 		cache[data.original_url] = data;
 	}).done(function() {
-		// ... and restore key that was before after request is done
+		// ... and restore key that was before when request is finished
 		$.embedly.defaults.key = embedlyKey;
 		respond();
 	});
