ca5e69e2f936ee11f5d8e6acb1af27ca66083ecd
Valeriy Shkiryak <lizard@aboutecho.com>
2014-05-16 17:43:08 +0400

    Replace Unicode quotes with ASCII ones

--- sdk-derived/controls/auth.js
+++ sdk-derived/controls/auth.js
@@ -22,7 +22,7 @@
  * 	});
  *
  * More information regarding the possible ways of the Control initialization
- * can be found in the [“How to initialize Echo components”](#!/guide/how_to_initialize_components-section-initializing-an-app) guide.
+ * can be found in the ["How to initialize Echo components"](#!/guide/how_to_initialize_components-section-initializing-an-app) guide.
  *
  * @extends Echo.Control
  *
