06aeca1a494dde47449d54745e1b2a4015b7c15b
Valeriy Shkiryak <lizard@aboutecho.com>
2014-07-25 15:34:10 +0400

    Use Echo.Utils.sanitize method defined in JS SDK

--- sdk-derived/utils.js
+++ sdk-derived/utils.js
@@ -34,11 +34,7 @@ Echo.Utils.addCSS(
 );
 
 Echo.Utils.sanitizeOEmbedHTML = function(html) {
-	// We intentionally break HTML attributes starting with "on".
-	// These attributes are only event handlers anyway and we don't want them.
-	// This action doesn't allow for "<img src=# onerror=alert(1)>" HTML string
-	// to execute JavaScript
-	html = html.replace(/(<[^>]+?\bon)([^>]+>)/ig, "$1_$2");
+	html = Echo.Utils.sanitize(html, "html");
 	html = $.parseHTML(html);
 	// extract only iframes, all other elements are possible hacks
 	return $(html).filter("iframe");
@@ -47,7 +43,7 @@ Echo.Utils.sanitizeOEmbedHTML = function(html) {
 Echo.Utils.sanitizeOEmbed = function(oembed) {
 	// Note that "html" field is not sanitized here as it's a heavy operation.
 	// Call Echo.Utils.sanitizeOEmbedHTML when html is going to be displayed.
-	var fields = {
+	var mandatoryFields = {
 		"photo": ["url", "width", "height"],
 		"link": ["url", "title"],
 		"video": ["html", "width", "height"],
@@ -57,36 +53,32 @@ Echo.Utils.sanitizeOEmbed = function(oembed) {
 	if (!$.isPlainObject(oembed) ||
 		!oembed.type ||
 		!oembed.version ||
-		!fields[oembed.type]
+		!mandatoryFields[oembed.type]
 	) {
 		return null;
 	}
 
-	// sanitize URL fields
-	$.each(["url", "original_url", "provider_url", "author_url", "thumbnail_url"], function(i, field) {
+	var fieldSanitizers = {
+		"url": "url",
+		"original_url": "url",
+		"provider_url": "url",
+		"author_url": "url",
+		"thumbnail_url": "url",
+		"height": "number",
+		"width": "number",
+		"thumbnail_width": "number",
+		"thumbnail_height": "number",
+		"title": "plainText",
+		"description": "plainText",
+		"author_name": "plainText",
+		"provider_name": "plainText"
+	};
+	$.each(fieldSanitizers, function(field, sanitizer) {
 		if (!oembed[field]) return;
-		if (/javascript:/i.test(oembed[field])) {
-			delete oembed[field];
-		}
+		oembed[field] = Echo.Utils.sanitize(oembed[field], sanitizer);
 	});
 
-	// sanitize numeric fields
-	$.each(["height", "width", "thumbnail_width", "thumbnail_height"], function(i, field) {
-		if (!oembed[field]) return;
-		oembed[field] = +oembed[field];
-		if (isNaN(oembed[field])) {
-			delete oembed[field];
-		}
-	});
-
-	// sanitize text-only fields
-	$.each(["title", "description", "author_name", "provider_name"], function(i, field) {
-		if (!oembed[field]) return;
-		oembed[field] = Echo.Utils.stripTags(oembed[field]);
-	});
-
-	// check if some necessary fields are missing
-	var missing = $.grep(fields[oembed.type], function(field) {
+	var missing = $.grep(mandatoryFields[oembed.type], function(field) {
 		return !oembed[field];
 	});
 	return missing.length ? null : oembed;
